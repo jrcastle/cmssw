e9abb184b1b86e1345aba1c1420af2fbedbc2466
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2015-04-16 15:15:47 +0200

    moved the beamspot workflow under python and rationalised the structure

---
+++ RecoVertex/BeamSpotProducer/python/workflow/objects/IOVObj.py
@@ -0,0 +1,11 @@
+class IOV(object):
+    '''
+    Interval of Validity object
+    '''
+    def __init__(self):
+        self.since = 1
+        self.till = 1
+        self.RunFirst = 1
+        self.RunLast  = 1
+        self.LumiFirst = 1
+        self.LumiLast = 1
