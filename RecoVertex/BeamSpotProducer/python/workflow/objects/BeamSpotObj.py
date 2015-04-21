6164a01f717c51045bab61fc6ffb7aa5c86e2839
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2015-04-21 15:32:35 +0200

    modified BeamSpotObj and added PayloadObj. It is now possible to read the ASCII file and istantiate a BeamspoObject for each single fit performed, and also the other way round, dump a BeamSpot object into a Payload ASCII file

--- RecoVertex/BeamSpotProducer/python/workflow/objects/BeamSpotObj.py
+++ RecoVertex/BeamSpotProducer/python/workflow/objects/BeamSpotObj.py
@@ -1,42 +1,133 @@
+#!/usr/bin/python
+
+import datetime
+
 class BeamSpot(object):
     '''
     BeamSpot object
     '''
     def __init__(self):
-        self.Type = -1
-        self.X = 0.
-        self.Xerr = 0.
-        self.Y = 0.
-        self.Yerr = 0.
-        self.Z = 0.
-        self.Zerr = 0.
-        self.sigmaZ = 0.
-        self.sigmaZerr = 0.
-        self.dxdz = 0.
-        self.dxdzerr = 0.
-        self.dydz = 0.
-        self.dydzerr = 0.
-        self.beamWidthX = 0.
-        self.beamWidthXerr = 0.
-        self.beamWidthY = 0.
-        self.beamWidthYerr = 0.
-        self.EmittanceX = 0.
-        self.EmittanceY = 0.
-        self.betastar = 0.
-        self.IOVfirst = 0
-        self.IOVlast = 0
-        self.IOVBeginTime = 0
-        self.IOVEndTime = 0
-        self.Run = 0
+        self.Reset()
+        
     def Reset(self):
-       self.Type = -1
-       self.X = self.Y = self.Z = 0.
-       self.Xerr = self.Yerr = self.Zerr = 0.
-       self.sigmaZ = self.sigmaZerr = 0.
-       self.dxdz = self.dydz = 0.
-       self.dxdzerr = self.dydzerr = 0.
-       self.beamWidthX = self.beamWidthY = 0.
-       self.beamWidthXerr = self.beamWidthYerr = 0.
-       self.EmittanceX = self.EmittanceY = self.betastar = 0.
-       self.IOVfirst = self.IOVlast = 0
-       self.Run = 0
+        # the names of the errors suggest they're not squared, 
+        # while they actually are!
+        self.Type          = -1
+        self.X             =  0.
+        self.Xerr          =  0.
+        self.Y             =  0.
+        self.Yerr          =  0.
+        self.Z             =  0.
+        self.Zerr          =  0.
+        self.sigmaZ        =  0.
+        self.sigmaZerr     =  0.
+        self.dxdz          =  0.
+        self.dxdzerr       =  0.
+        self.dydz          =  0.
+        self.dydzerr       =  0.
+        self.beamWidthX    =  0.
+        self.beamWidthXerr =  0.
+        self.beamWidthY    =  0.
+        self.beamWidthYerr =  0.
+        self.EmittanceX    =  0.
+        self.EmittanceY    =  0.
+        self.betastar      =  0.
+        self.IOVfirst      =  0
+        self.IOVlast       =  0
+        self.IOVBeginTime  =  0
+        self.IOVEndTime    =  0
+        self.Run           =  0
+        self.XYerr         =  0. 
+        self.YXerr         =  0.
+        self.dxdzdydzerr   =  0.
+        self.dydzdxdzerr   =  0.
+
+
+    def Dump(self, file, mode = 'a'):
+        
+        f = open(file, mode)
+        
+        date_start = datetime.datetime.utcfromtimestamp(self.IOVBeginTime)
+        date_end   = datetime.datetime.utcfromtimestamp(self.IOVEndTime  )
+        
+        #import pdb ; pdb.set_trace()
+        
+        str_date_start = '%d.%02d.%02d %02d:%02d:%02d' %( date_start.year   ,
+                                                          date_start.month  ,
+                                                          date_start.day    ,
+                                                          date_start.hour   ,
+                                                          date_start.minute ,
+                                                          date_start.second )
+        
+        str_date_end   = '%d.%02d.%02d %02d:%02d:%02d' %( date_end.year   ,
+                                                          date_end.month  ,
+                                                          date_end.day    ,
+                                                          date_end.hour   ,
+                                                          date_end.minute ,
+                                                          date_end.second )
+        
+        towrite = 'Runnumber {RUN}\n'                                  \
+                  'BeginTimeOfFit {DATESTART} GMT {TIMESTART}\n'       \
+                  'EndTimeOfFit {DATEEND} GMT {TIMEEND}\n'             \
+                  'LumiRange {LUMISTART} - {LUMIEND}\n'                \
+                  'Type {TYPE}\n'                                      \
+                  'X0 {X0}\n'                                          \
+                  'Y0 {Y0}\n'                                          \
+                  'Z0 {Z0}\n'                                          \
+                  'sigmaZ0 {SZ0}\n'                                    \
+                  'dxdz {DXDZ}\n'                                      \
+                  'dydz {DYDZ}\n'                                      \
+                  'BeamWidthX {BWX}\n'                                 \
+                  'BeamWidthY {BWY}\n'                                 \
+                  'Cov(0,j) {M00} {M01} 0 0 0 0 0 \n'                  \
+                  'Cov(1,j) {M10} {M11} 0 0 0 0 0 \n'                  \
+                  'Cov(2,j) 0 0 {M22} 0 0 0 0 \n'                      \
+                  'Cov(3,j) 0 0 0 {M33} 0 0 0 \n'                      \
+                  'Cov(4,j) 0 0 0 0 {M44} {M45} 0 \n'                  \
+                  'Cov(5,j) 0 0 0 0 {M54} {M55} 0 \n'                  \
+                  'Cov(6,j) 0 0 0 0 0 0 {M66} \n'                      \
+                  'EmittanceX {EMX}\n'                                 \
+                  'EmittanceY {EMY}\n'                                 \
+                  'BetaStar {BSTAR}\n'                                 \
+                  ''.format(RUN       = str(self.Run          ),
+                            DATESTART = str(str_date_start    ),
+                            TIMESTART = str(self.IOVBeginTime ),
+                            DATEEND   = str(str_date_end      ),
+                            TIMEEND   = str(self.IOVEndTime   ),
+                            LUMISTART = str(self.IOVfirst     ),
+                            LUMIEND   = str(self.IOVlast      ),
+                            TYPE      = str(self.Type         ),
+                            X0        = str(self.X            ),
+                            Y0        = str(self.Y            ),
+                            Z0        = str(self.Z            ),
+                            SZ0       = str(self.sigmaZ       ),
+                            DXDZ      = str(self.dxdz         ),
+                            DYDZ      = str(self.dydz         ),
+                            BWX       = str(self.beamWidthX   ),
+                            BWY       = str(self.beamWidthY   ),
+                            # diagonal
+                            M00       = str(self.Xerr         ),
+                            M11       = str(self.Yerr         ),
+                            M22       = str(self.Zerr         ),
+                            M33       = str(self.sigmaZerr    ),
+                            M44       = str(self.dxdzerr      ),
+                            M55       = str(self.dydzerr      ),
+                            M66       = str(self.beamWidthXerr),
+                            # off diagonal
+                            M01       = str(self.XYerr        ),
+                            M10       = str(self.YXerr        ),
+                            M45       = str(self.dxdzdydzerr  ),
+                            M54       = str(self.dydzdxdzerr  ),
+                            EMX       = str(self.EmittanceX   ),
+                            EMY       = str(self.EmittanceY   ),
+                            BSTAR     = str(self.betastar     ),
+                            )
+        
+        f.write(towrite)
+       
+if __name__ == '__main__':
+    mybs = BeamSpot()
+    mybs.Dump('bs_dump_dummy.txt', 'w+')
+
+
+                  
---
+++ RecoVertex/BeamSpotProducer/python/workflow/objects/PayloadObj.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+
+import math
+from RecoVertex.BeamSpotProducer.workflow.objects.BeamSpotObj import BeamSpot
+
+class Payload(object):
+    '''
+    '''
+    
+    def __init__(self, files):
+        '''
+        '''
+        # can pass a single or a list of txt files
+        if not isinstance(files, (list, tuple)):
+            files = [files]
+        
+        #self.run     = str(run)
+        self.inFiles = files
+        self._readFiles(files)
+    
+    def _readFiles(self, files):
+        '''
+        '''
+        lines = []
+        
+        for f in files:
+            #if self.run not in f:
+            #    print 'ERROR: check that the run corresponds to the files'
+            with open(f, 'r') as file:
+                for line in file:
+                    lines.append(line) 
+        
+        self.lines = lines
+    
+    def writeFile(self, payload):
+        '''
+        '''
+        pass
+    
+    def splitBySingleFit(self):
+        '''
+        '''
+        singleFits = {}
+        
+        for i, line in enumerate(self.lines):
+            line = line.rstrip()
+            if 'LumiRange' in line:
+                singleFits[line] = [self.lines[j].rstrip() \
+                                    for j in range(i-3, i+20)]
+        
+        return singleFits    
+
+    def fromTextToBS(self):
+        '''
+        Return a dictionary of dictionaries, as the following:
+        { Run : {Lumi Range: BeamSpot Fit Object} }
+        
+        Parses the files passed when the Payload is istantiated
+        '''
+        
+        singleFits = self.splitBySingleFit()
+        
+        beamspots = {}
+        
+        for k, v in singleFits.items():
+            
+            bs = BeamSpot()
+
+            bs.Run           = int  ( v[ 0].split()[1] )
+            bs.IOVBeginTime  = int  ( v[ 1].split('GMT')[1] )
+            bs.IOVEndTime    = int  ( v[ 2].split('GMT')[1] )
+            bs.IOVfirst      = int  ( v[ 3].split()[1] )
+            bs.IOVlast       = int  ( v[ 3].split()[3] )
+            bs.Type          = int  ( v[ 4].split()[1] )
+            
+            bs.X             = float( v[ 5].split()[1] )
+            bs.Y             = float( v[ 6].split()[1] )
+            bs.Z             = float( v[ 7].split()[1] )
+
+            bs.sigmaZ        = float( v[ 8].split()[1] )
+            bs.dxdz          = float( v[ 9].split()[1] )
+            bs.dydz          = float( v[10].split()[1] )
+
+            bs.beamWidthX    = float( v[11].split()[1] )
+            bs.beamWidthY    = float( v[12].split()[1] )
+            
+            # covariance matrix defined here
+            # https://github.com/MilanoBicocca-pix/cmssw/blob/CMSSW_7_5_X_beamspot_workflow_riccardo/RecoVertex/BeamSpotProducer/src/PVFitter.cc#L306
+            # diagonal terms
+            bs.Xerr          = float( v[13].split()[1] )
+            bs.Yerr          = float( v[14].split()[2] )
+            bs.Zerr          = float( v[15].split()[3] )
+            bs.sigmaZerr     = float( v[16].split()[4] )
+            bs.dxdzerr       = float( v[17].split()[5] )
+            bs.dydzerr       = float( v[18].split()[6] )
+            bs.beamWidthXerr = float( v[19].split()[7] )
+            # bs.beamWidthYerr = math.sqrt(float( v[16].split()[1] ) ) # not in cov matrix!
+            # off diagonal terms
+            bs.XYerr         = float( v[13].split()[2] )
+            bs.YXerr         = float( v[14].split()[1] )
+            bs.dxdzdydzerr   = float( v[17].split()[6] )
+            bs.dydzdxdzerr   = float( v[18].split()[5] )
+                
+            bs.EmittanceX    = float( v[20].split()[1] )
+            bs.EmittanceY    = float( v[21].split()[1] )
+
+            bs.betastar      = float( v[22].split()[1] )
+
+            try:   
+                beamspots[bs.Run][str('%d-%d' %(bs.IOVfirst, bs.IOVlast))] = bs
+            except:
+                toadd = { bs.Run : {str('%d-%d' %(bs.IOVfirst, bs.IOVlast)) : bs} }
+                beamspots.update( toadd )
+                   
+        return beamspots
+            
+
+if __name__ == '__main__':
+    
+    myPL = Payload('/afs/cern.ch/work/m/manzoni/beamspot/CMSSW_7_4_0_pre8/src/'\
+                   'RecoVertex/BeamSpotProducer/python/workflow/cfg/'          \
+                   'Runs2012B_FULL/Results/XRepFinal_1_195660_1.txt'           )
+
+    allLines = myPL.splitBySingleFit()
+    
+    allBs = myPL.fromTextToBS()
+    allBs[195660]['60-60'].Dump('bs_dump_195660_LS60.txt', 'w+')
