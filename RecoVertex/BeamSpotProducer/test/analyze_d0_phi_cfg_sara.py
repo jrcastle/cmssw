25af8ab71a1fa6f94173ae59fdce717c79c17f41
sara <sara.fiorendi@cern.ch>
2015-05-04 16:14:43 +0200

    save correct parameters for d0 fit into my cfg

--- RecoVertex/BeamSpotProducer/test/analyze_d0_phi_cfg_sara.py
+++ RecoVertex/BeamSpotProducer/test/analyze_d0_phi_cfg_sara.py
@@ -9,14 +9,16 @@ from fileList_StreamExpressPCL import *
 process.source = cms.Source("PoolSource",
                    fileNames = cms.untracked.vstring(
 #                                streamExpressList_Old
-                "file:/afs/cern.ch/work/f/fiorendi/private/BeamSpot/CMSSW_7_4_0/src/skim_Tier0_PCLTest_SUPERBUNNIES_vocms047_StreamExpressv3_alcareco_lumi690-710.root",
-                "file:/afs/cern.ch/work/f/fiorendi/private/BeamSpot/CMSSW_7_4_0/src/skim_Tier0_PCLTest_SUPERBUNNIES_vocms047_StreamExpressv3_alcareco_lumi690-710_part2.root",
+#                 "file:/afs/cern.ch/work/f/fiorendi/private/BeamSpot/CMSSW_7_4_0/src/skim_Tier0_PCLTest_SUPERBUNNIES_vocms047_StreamExpressv3_alcareco_lumi690-710.root",
+#                 "file:/afs/cern.ch/work/f/fiorendi/private/BeamSpot/CMSSW_7_4_0/src/skim_Tier0_PCLTest_SUPERBUNNIES_vocms047_StreamExpressv3_alcareco_lumi690-710_part2.root",
+                "file:/afs/cern.ch/work/f/fiorendi/private/BeamSpot/CMSSW_7_4_0/src/skim_Tier0_PCLTest_SUPERBUNNIES_vocms047_StreamExpressv3_alcareco_lumi500-900.root",
 #                 "file:/afs/cern.ch/work/f/fiorendi/private/BeamSpot/CMSSW_7_4_0/src/skim_StreamExpress2012D_lumi690-710_part1.root",
 #                 "file:/afs/cern.ch/work/f/fiorendi/private/BeamSpot/CMSSW_7_4_0/src/Configuration/DataProcessing/test/TkAlMinBias.root"
 			        )
 			     )
 
-process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange('207231:690-207231:710')
+# process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange('207231:500-207231:900')
+# process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange('207231:690-207231:710')
 
 process.maxEvents = cms.untracked.PSet(
                                        input = cms.untracked.int32(-1) #1500
@@ -54,16 +56,16 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 #process.d0_phi_analyzer.BeamFitter.TrackCollection = cms.untracked.InputTag('ALCARECOTkAlMinBias') #,'UpdatedAtVtx')
 
 process.d0_phi_analyzer.BeamFitter.WriteAscii  	             = True
-process.d0_phi_analyzer.BeamFitter.AsciiFileName  	         = 'BeamFit_207231_PCLReplay_redo_sara.txt'
+process.d0_phi_analyzer.BeamFitter.AsciiFileName  	         = 'BeamFit_207231_PCLReplay_Lumi500_900.txt'
 process.d0_phi_analyzer.BeamFitter.Debug                     = True
 process.d0_phi_analyzer.BeamFitter.InputBeamWidth            = -1
-# process.d0_phi_analyzer.BeamFitter.MaximumImpactParameter    = 1.0 # diff from alca
+process.d0_phi_analyzer.BeamFitter.MaximumImpactParameter    = 1.0 # diff from alca
 process.d0_phi_analyzer.BeamFitter.MaximumNormChi2    	     = 10
-# process.d0_phi_analyzer.BeamFitter.MinimumInputTracks 	     = 2   # diff from alca
+process.d0_phi_analyzer.BeamFitter.MinimumInputTracks 	     = 50   # diff from alca
 process.d0_phi_analyzer.BeamFitter.MinimumPixelLayers 	     = -1
 process.d0_phi_analyzer.BeamFitter.MinimumPt                 = 1.0
 process.d0_phi_analyzer.BeamFitter.MinimumTotalLayers 	     = 6
-process.d0_phi_analyzer.BeamFitter.OutputFileName 	         = 'run207231_PCLReplay_addCombinedResultsTree.root' #AtVtx10000.root'
+process.d0_phi_analyzer.BeamFitter.OutputFileName 	         = 'run207231_PCLReplay_Lumi500_900_addCombinedResultsTree.root' #AtVtx10000.root'
 process.d0_phi_analyzer.BeamFitter.TrackAlgorithm            = cms.untracked.vstring()
 #process.d0_phi_analyzer.BeamFitter.TrackQuality             = cms.untracked.vstring("highPurity")
 process.d0_phi_analyzer.BeamFitter.SaveFitResults 	         = True
