d3f8557be96e22031f0e60016ce66f7a3e065853
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2015-06-15 18:43:05 +0200

    generalised IOV object and allowed plotting with LS ranges in PAyload object

--- RecoVertex/BeamSpotProducer/python/workflow/objects/IOVObj.py
+++ RecoVertex/BeamSpotProducer/python/workflow/objects/IOVObj.py
@@ -2,10 +2,24 @@ class IOV(object):
     '''
     Interval of Validity object
     '''
-    def __init__(self):
-        self.since     = 1
-        self.till      = 1
-        self.RunFirst  = 1
-        self.RunLast   = 1
-        self.LumiFirst = 1
-        self.LumiLast  = 1
+    def __init__(self, *args):
+        '''
+        IOV() initiates a dummy IOV, all values set to -1.
+        IOV(BeamSpot) initiates a IOV object with, run, LS and 
+        time boundaries taken from the given BeamSpot object.
+        Inly positional call (for now)
+        '''
+        self.since     = -1
+        self.till      = -1
+        self.RunFirst  = -1
+        self.RunLast   = -1
+        self.LumiFirst = -1
+        self.LumiLast  = -1
+        
+        if len(args):
+            self.since     = args[0].IOVBeginTime
+            self.till      = args[0].IOVEndTime
+            self.RunFirst  = args[0].Run
+            self.RunLast   = args[0].Run
+            self.LumiFirst = args[0].IOVfirst
+            self.LumiLast  = args[0].IOVlast
--- RecoVertex/BeamSpotProducer/python/workflow/objects/PayloadObj.py
+++ RecoVertex/BeamSpotProducer/python/workflow/objects/PayloadObj.py
@@ -3,6 +3,7 @@
 import ROOT
 from array import array
 from RecoVertex.BeamSpotProducer.workflow.objects.BeamSpotObj import BeamSpot
+from RecoVertex.BeamSpotProducer.workflow.objects.IOVObj import IOV
 
 class Payload(object):
     '''
@@ -49,7 +50,7 @@ class Payload(object):
         
         return singleFits    
 
-    def fromTextToBS(self):
+    def fromTextToBS(self, iov = False):
         '''
         Return a dictionary of dictionaries, as the following:
         { Run : {Lumi Range: BeamSpot Fit Object} }
@@ -65,17 +66,22 @@ class Payload(object):
             
             bs = BeamSpot()
             bs.Read(v)
+            
+            if iov:
+                keyIOV = IOV(bs) # IOV with the given BS boundaries            
+                beamspots[keyIOV] = bs
 
-            if bs.IOVfirst == bs.IOVlast:
-                lsrange = bs.IOVfirst
             else:
-                lsrange = '%d-%d' %(bs.IOVfirst, bs.IOVlast)
+                if bs.IOVfirst == bs.IOVlast:
+                    lsrange = bs.IOVfirst
+                else:
+                    lsrange = '%d-%d' %(bs.IOVfirst, bs.IOVlast)
                 
-            try:   
-                beamspots[bs.Run][lsrange] = bs
-            except:
-                toadd = { bs.Run : {lsrange : bs} }
-                beamspots.update( toadd )
+                try:   
+                    beamspots[bs.Run][lsrange] = bs
+                except:
+                    toadd = { bs.Run : {lsrange : bs} }
+                    beamspots.update( toadd )
                    
         return beamspots
     
@@ -109,17 +115,19 @@ class Payload(object):
     def plot(self, variable, run, iLS = -1, fLS = 1e6, savePdf = False):
         '''
         For a given run, plot a BS parameter as a function of LS.
+        Allows multiple LS bins.
         '''
         # get the list of BS objects
-        myBS = {k:v for k, v in self.fromTextToBS()[run].items() 
-                if k >= iLS and k <= fLS}
-        
-        nBins = len(myBS.keys())
-        
-        x = array('f', myBS.keys())
+        myBS = {k:v for k, v in self.fromTextToBS(iov = True).items()
+                if k.RunFirst == run and k.RunLast == run 
+                and k.LumiFirst >= iLS and k.LumiLast <= fLS}
+
+        nBins = len(myBS)
+                
+        x = array('f', [0.5 * (k.LumiLast + k.LumiFirst) for k in myBS.keys()])
         y = array('f', [getattr(v, variable) for v in myBS.values()])
         
-        xe = array('f', [0.5 for k in myBS.keys()])
+        xe = array('f', [0.5 * max(1, (k.LumiLast - k.LumiFirst)) for k in myBS.keys()])
         ye = array('f', [getattr(v, variable + 'err') for v in myBS.values()])
         
         tge = ROOT.TGraphErrors(nBins, x, y, xe, ye)
@@ -127,17 +135,24 @@ class Payload(object):
         tge.SetTitle('Run ' + str(run))
         tge.GetYaxis().SetTitle(variable)
         tge.GetXaxis().SetTitle('Lumi Section')
-        
+        tge.SetMarkerStyle(8)
+#         tge.SetMarkerSize(5.)
+       
         c1 = ROOT.TCanvas('','',1000,700)
         tge.Draw('AP')
         if savePdf: 
             c1.SaveAs('BS_plot_%d_%s.pdf' %(run, variable))
 
+
 if __name__ == '__main__':
 
-    file = '/afs/cern.ch/user/m/manzoni/public/beamspot_validation/' \
-           'BeamFit_LumiBased_NewAlignWorkflow_alcareco_Run247388.txt'
+    #file = '/afs/cern.ch/user/m/manzoni/public/beamspot_validation/' \
+    #       'BeamFit_LumiBased_NewAlignWorkflow_alcareco_Run247388.txt'
 
+
+    file = '/afs/cern.ch/user/f/fiorendi/public/beamSpot/'\
+           'beamspot_firstData_run247324_byLumi_all_lumi98_107.txt'
+    file = '/afs/cern.ch/user/f/fiorendi/public/beamSpot/bs_weighted_results_246908.txt'
     myPL = Payload(file)
     
     #myPL = Payload('/afs/cern.ch/work/m/manzoni/beamspot/CMSSW_7_4_0_pre8/src/'\
@@ -148,18 +163,21 @@ if __name__ == '__main__':
     allLines = myPL.splitBySingleFit()
     
     allBs = myPL.fromTextToBS()
+
+    allBs2 = myPL.fromTextToBS(iov = True)
+    
     #allBs[195660][60].Dump('bs_dump_195660_LS60.txt', 'w+')
-    allBs[247388][60].Dump('bs_dump_247388_LS60.txt', 'w+')
+    #allBs[247324][98].Dump('bs_dump_247388_LS60.txt', 'w+')
     
     print myPL.getProcessedLumiSections()
 
-    myPL.plot('X'         , 247388, savePdf = True)
-    myPL.plot('Y'         , 247388, savePdf = True)
-    myPL.plot('Z'         , 247388, savePdf = True)
-    myPL.plot('sigmaZ'    , 247388, savePdf = True)
-    myPL.plot('dxdz'      , 247388, savePdf = True)
-    myPL.plot('dydz'      , 247388, savePdf = True)
-    myPL.plot('beamWidthX', 247388, savePdf = True)
-    myPL.plot('beamWidthY', 247388, savePdf = True)
+    myPL.plot('X'         , 246908, savePdf = True)
+    myPL.plot('Y'         , 246908, savePdf = True)
+    myPL.plot('Z'         , 246908, savePdf = True)
+    myPL.plot('sigmaZ'    , 246908, savePdf = True)
+    myPL.plot('dxdz'      , 246908, savePdf = True)
+    myPL.plot('dydz'      , 246908, savePdf = True)
+    myPL.plot('beamWidthX', 246908, savePdf = True)
+    myPL.plot('beamWidthY', 246908, savePdf = True)
 
-    myPL.plot('X'         , 247388, iLS = 50, fLS = 55, savePdf = True)
+    myPL.plot('X'         , 246908, iLS = 90, fLS = 110, savePdf = True)
